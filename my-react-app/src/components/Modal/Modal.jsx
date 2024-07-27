284f5d6c52b459c88ccf00ff138c9799c02aedd1
michael-loopli <michael.lane@getloopli.com>
2024-07-27 15:24:11 +0100

    components for Modal

---
+++ my-react-app/src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import "./Modal.css";
+
+const Modal = ({ message, onClose }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  message: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
